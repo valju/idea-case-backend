3059962979287ebde2988731544dfd216a034da9
Mia Tran <mia@Mias-MacBook-Pro.local>
2019-04-22 01:15:30 +0300

    update and delete

--- src/routes/api/category.js
+++ src/routes/api/category.js
@@ -1,16 +1,19 @@
-import express from 'express';
-import knex from '../../db/index';
+import express from "express";
+import knex from "../../db/index";
 
 const category = express.Router();
 
 // These have been tested to work!!! 2019-04-12
 
 /** http://localhost:8787/api/category/all    with method=GET **/
-category.get('/all', function (req, res) {
-  knex.select().from('Category').then((data) => {
-    res.status(200);
-    res.send(data);
-  });
+category.get("/all", function(req, res) {
+  knex
+    .select()
+    .from("Category")
+    .then(data => {
+      res.status(200);
+      res.send(data);
+    });
 });
 
 /** http://localhost:8787/api/category/    with method=GET **/
@@ -24,60 +27,118 @@ category.get('/', function (req, res) {
 });
 */
 
-// http://localhost:8787/api/category/1 
-category.get('/:id', function (req, res) {
-  knex.select().from('Category').where('id',req.params.id).then((data) => {
-    res.status(200);
-    res.send(data);
-  });
+// http://localhost:8787/api/category/1
+category.get("/:id", function(req, res) {
+  knex
+    .select()
+    .from("Category")
+    .where("id", req.params.id)
+    .then(data => {
+      res.status(200);
+      res.send(data);
+    });
 });
 
 /** http://localhost:8787/api/category/    with method=POST **/
-category.post('/', function (req, res) {
-  // Just a start of err handling for model for you 
+category.post("/", function(req, res) {
+  // Just a start of err handling for model for you
   if (req.body.name && req.body.description) {
-    knex.insert(req.body).returning('*').into('Category')
-    .then(
-      (data) => {
+    knex
+      .insert(req.body)
+      .returning("*")
+      .into("Category")
+      .then(data => {
         res.status(200);
         res.send(data);
-      }
-    ).catch ((error) => {
-        if(error.errno == 1062) {  // https://mariadb.com/kb/en/library/mariadb-error-codes/
+      })
+      .catch(error => {
+        if (error.errno == 1062) {
+          // https://mariadb.com/kb/en/library/mariadb-error-codes/
           res.status(409);
           res.send("Conflict: Category with that name already exists!");
         } else {
           res.status(400);
-          res.send("Database error, Error number: " +error.errno);
-        }      
-    });
+          res.send("Database error, Error number: " + error.errno);
+        }
+      });
   } else {
     res.status(400);
-    res.end(JSON.stringify({
-      "error": "firstName and/or lastName missing"
-    }));
+    res.end(
+      JSON.stringify({
+        error: "firstName and/or lastName missing"
+      })
+    );
   }
 });
 
 /** http://localhost:8787/api/category/insertMany    with method=POST **/
-category.post('/insertMany', function (req, res) {
-    knex.insert(req.body).returning('*').into('Category').then(
-      (data) => {
-        res.status(200);
-        res.send(data);
+category.post("/insertMany", function(req, res) {
+  knex
+    .insert(req.body)
+    .returning("*")
+    .into("Category")
+    .then(data => {
+      res.status(200);
+      res.send(data);
+    })
+    .catch(error => {
+      if (error.errno == 1062) {
+        // https://mariadb.com/kb/en/library/mariadb-error-codes/
+        res.status(409);
+        res.send("Conflict: Category with that name already exists!");
+      } else {
+        res.status(400);
+        res.send("Database error, Error number: " + error.errno);
       }
-    ).catch ((error)=> {
-        if(error.errno == 1062) {  // https://mariadb.com/kb/en/library/mariadb-error-codes/
-          res.status(409);
-          res.send("Conflict: Category with that name already exists!");
-        } else {
-          res.status(400);
-          res.send("Database error, Error number: " +error.errno);
-        }      
     });
 });
 
-// The req.body.name etc. parameter check would work, but 
+category.delete("/deleteOne", (req, res) => {
+  if (!req.body.id || isNaN(req.body.id)) {
+    res.status(422);
+    res.send("invalid input");
+  } else {
+    knex("Category")
+      .where("id", req.body.id)
+      .del()
+      .then(data => {
+        console.log(data);
+        if (data === 0) {
+          res.status(400);
+          res.send("id does not exist");
+        } else {
+          res.status(200);
+          res.send("delete ok");
+        }
+      })
+      .catch(() => {
+        res.status(400);
+        res.send("delete error ");
+      });
+  }
+});
+
+category.put("/update", (req, res) => {
+  const newCategory = {
+    id: req.body.id,
+    name: req.body.name ? req.body.name : "TBA",
+    description: req.body.description ? req.body.description : "TBA",
+    budgetLimit: req.body.budgetLimit ? req.body.budgetLimit : 0,
+    isActive: req.body.isActive ? req.body.isActive : 0
+  };
+  knex("Category")
+    .where("id", req.body.id)
+    .update(newCategory)
+    .then(() => {
+      res.status(200);
+      res.send("edit ok");
+    })
+    .catch(() => {
+      res.status(400);
+      res.send("edit not ok");
+    });
+});
+// The req.body.name etc. parameter check would work, but
 // naturally not with the multi-insert with JSON array below
 
 export default category;
