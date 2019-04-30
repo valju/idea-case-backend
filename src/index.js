8cd6f951a39b7157bfe34e22a4b65c8b1f407775
Ha Van <vannunganha@gmail.com>
2019-04-30 11:24:45 +0300

    create GET/POST/PUT/DELETE method for idea member

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "@babel/polyfill";
-import express from 'express';
-import cors from 'cors';
-import routes from './routes/api/index';
-import bodyParser from 'body-parser';
+import express from "express";
+import cors from "cors";
+import routes from "./routes/api/index";
+import bodyParser from "body-parser";
 
-import {SERVER_SETTINGS} from './CONSTANTS';
+import { SERVER_SETTINGS } from "./CONSTANTS";
 
 const app = express();
 
@@ -14,15 +14,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const init = async () => {
   await app.use(SERVER_SETTINGS.api_url_prefix, routes);
-  
-  await app.get('/', function (req, res) {
-    res.send('Hello from the Node&Express Backend!').end();
+
+  await app.get("/", function(req, res) {
+    res.send("Hello from the Node&Express Backend!").end();
+  });
+
+  await app.use((error, req, res, next) => {
+    return res.status(error.status || 500).json({ error: error.message });
   });
 
   await app.listen(SERVER_SETTINGS.port);
-  
+
   console.log(`Node server started and listens to \
-              port ${SERVER_SETTINGS.port}.`); 
-}
+              port ${SERVER_SETTINGS.port}.`);
+};
 
 init();
--- src/routes/api/ideaMember.js
+++ src/routes/api/ideaMember.js
@@ -1,5 +1,86 @@
 import express from "express";
+import knex from "../../db/index";
 
 const ideaMember = express.Router();
+
+/* GET all idea member */
+ideaMember.get("/all", (req, res) => {
+  knex
+    .select()
+    .from("Idea_member")
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// POST idea member
+ideaMember.post("/", (req, res, next) => {
+  let { ideaId, memberId } = req.body;
+  return knex
+    .select("ideaId")
+    .from("Idea_member")
+    .where({ ideaId })
+    .then(ideaList => {
+      if (ideaList.length === 0) {
+        return knex
+          .insert({ ideaId, memberId })
+          .returning("*")
+          .into("Idea_member");
+      } else {
+        throw new Error("The idea member is already existed");
+      }
+    })
+    .then(data => {
+      console.log({ data });
+      return res.status(200).json({ success: "Idea member inserted" });
+    })
+    .catch(err => {
+      if (err.message === "The idea member is already existed") {
+        const error = new Error(err.message);
+        error.status = 409;
+        next(error);
+      } else {
+        next(err);
+      }
+    });
+});
+
+// PUT update idea member
+ideaMember.put("/", (req, res, next) => {
+  let { ideaId, memberId } = req.body;
+  return knex
+    .select("ideaId")
+    .from("Idea_member")
+    .where({ ideaId })
+    .then(ideaList => {
+      if (ideaList.length > 0) {
+        return knex("Idea_member")
+          .where({ ideaId })
+          .update({ memberId });
+      } else {
+        throw new Error("Cannot find ideamember to update");
+      }
+    })
+    .then(() => {
+      return res.status(200).json({ success: "Idea member updated" });
+    })
+    .catch(err => {
+      if (err.message === "Cannot find ideamember to update") {
+        const error = new Error(err.message);
+        error.status(404);
+        next(error);
+      } else {
+        next(err);
+      }
+    });
+});
+
+ideaMember.delete("/:id", (req, res, next) => {
+  let id = req.params.id;
+  knex("Idea_member")
+    .where("ideaId", id)
+    .del()
+    .then(data => res.status(200).json({ success: "Idea member deleted" }))
+    .catch(err => next(err));
+});
 
 export default ideaMember;
