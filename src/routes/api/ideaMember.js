a2c30b2bb0066442edf054017b01c5f0cd37b668
Linh Pham <linhpt199@gmail.com>
2019-05-02 16:04:42 +0300

    Fix POST & PUT method of idea_member

--- src/routes/api/ideaMember.js
+++ src/routes/api/ideaMember.js
@@ -3,7 +3,8 @@ import knex from "../../db/index";
 
 const ideaMember = express.Router();
 
-/* GET all idea member */
+// GET all idea member 
+// GET http://localhost:8787/api/ideaMember/all
 ideaMember.get("/all", (req, res) => {
   knex
     .select("ideaId", "memberId", "firstName", "lastName", "name")
@@ -15,14 +16,16 @@ ideaMember.get("/all", (req, res) => {
 });
 
 // POST idea member
+// POST http://localhost:8787/api/ideaMember/
+// example request body { "ideaId": "1001", "memberId": "101" }
 ideaMember.post("/", (req, res, next) => {
   let { ideaId, memberId } = req.body;
   return knex
-    .select("ideaId")
+    .select("ideaId", "memberId")
     .from("Idea_Member")
-    .where({ ideaId })
-    .then(ideaList => {
-      if (ideaList.length === 0) {
+    .where({ ideaId,  memberId })
+    .then(list => {
+      if (list.length === 0) {
         return knex
           .insert({ ideaId, memberId })
           .returning("*")
@@ -45,34 +48,101 @@ ideaMember.post("/", (req, res, next) => {
     });
 });
 
-// PUT update idea member
+// PUT update member in idea-member
 ideaMember.put("/", (req, res, next) => {
-  let { ideaId, memberId } = req.body;
-  return knex
-    .select("ideaId")
+  let { oldIdeaId, newIdeaId, oldMemberId, newMemberId } = req.body;
+
+  //*** Update only memberId OR ideaId once at a time
+  // Update memberId condition
+  const MemberId_updateCondition = (newIdeaId === null) 
+                                  && (newMemberId !== null) 
+                                  && (Number(newMemberId) !== NaN) // no inputs like { "newMemberId": "abc"} 
+                                  && (Number(newMemberId) !== 0) // no inputs like { "newMemberId": "" }
+
+  // Update ideaId condition
+  const IdeaId_updateCondition = (newMemberId === null) 
+                                  && (newIdeaId !== null) 
+                                  && (Number(newIdeaId) !== NaN) // no inputs like { "newIdeaId": "xyz" }
+                                  && (Number(newIdeaId) !== 0) // no inputs like { "newIdeaId": "" }
+
+  // Update memberId
+  if (MemberId_updateCondition) {
+    return knex
+    .select("ideaId", "memberId")
     .from("Idea_Member")
-    .where({ ideaId })
-    .then(ideaList => {
-      if (ideaList.length > 0) {
+    .where({ 
+      ideaId: oldIdeaId, 
+      memberId: oldMemberId 
+    })
+    .then(list => {
+      if (list.length > 0) {
         return knex("Idea_Member")
-          .where({ ideaId })
-          .update({ memberId });
+          .where({ 
+            ideaId: oldIdeaId,
+            memberId: oldMemberId 
+          })
+          .update({ memberId: newMemberId });
       } else {
-        throw new Error("Cannot find ideamember to update");
+        throw new Error("Cannot find idea-member to update");
       }
     })
     .then(() => {
-      return res.status(200).json({ success: "Idea member updated" });
+      return res.status(200).json({ success: "Member of an idea-member updated" });
     })
     .catch(err => {
-      if (err.message === "Cannot find ideamember to update") {
+      if (err.message === "Cannot find idea-member to update") {
         const error = new Error(err.message);
-        error.status(404);
+        res.status(404).end(error.message);
         next(error);
       } else {
         next(err);
       }
     });
+
+  //Update ideaId
+  } else if (IdeaId_updateCondition) {
+      return knex
+      .select("ideaId", "memberId")
+      .from("Idea_Member")
+      .where({ 
+        ideaId: oldIdeaId, 
+        memberId: oldMemberId 
+      })
+      .then(list => {
+        if (list.length > 0) {
+          return knex("Idea_Member")
+            .where({ 
+              ideaId: oldIdeaId,
+              memberId: oldMemberId 
+            })
+            .update({ ideaId: newIdeaId });
+        } else {
+          throw new Error("Cannot find idea-member to update");
+        }
+      })
+      .then(() => {
+        return res.status(200).json({ success: "Member of an idea-member updated" });
+      })
+      .catch(err => {
+        if (err.message === "Cannot find idea-member to update") {
+          const error = new Error(err.message);
+          res.status(404).end(error.message);
+          next(error);
+        } else {
+          next(err);
+        }
+      });
+  } else {
+    const errorMessage_wrongInputs = "Inputs are not correct! "
+                                    + "Inputs' length cannot be 0. "
+                                    + "Inputs CANNOT be NULL (except for memberId & ideaId). "
+                                    + "ideaId and numberId CANNOT be NULL at the same time. "                    
+                                    + "Inputs have to contain a string of number."
+    const error = new Error(errorMessage_wrongInputs)
+    res.status(422).end(error.message)
+  }
+  
+    
 });
 
 // DELETE delete idea member
